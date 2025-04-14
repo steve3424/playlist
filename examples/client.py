a95f201d07001dfba946333a67c296c02d5808d1
Steve F <stevefrazee123@gmail.com>
2025-04-13 20:14:29 -0400

    update client sample to get ticket from http server

--- examples/client.py
+++ examples/client.py
@@ -6,6 +6,7 @@ import logging
 import logging.config
 from logging_conf import LOGGING_CONFIG
 logging.config.dictConfig(LOGGING_CONFIG)
+import requests
 import asyncio
 import aioconsole
 from websockets.exceptions import ConnectionClosedOK, ConnectionClosedError, ConnectionClosed
@@ -13,7 +14,7 @@ from websockets.asyncio.client import connect, ClientConnection
 
 LOGGER = logging.getLogger(__name__)
 
-async def ws_listener(websocket: ClientConnection):
+async def wsListener(websocket: ClientConnection):
     LOGGER.info(f"Listening to {websocket.remote_address} for messages...")
     try:
         while True:
@@ -22,11 +23,11 @@ async def ws_listener(websocket: ClientConnection):
     except ConnectionClosedOK as ex:
         LOGGER.info(f"ConnectionClosedOk from server: {ex}")
     except ConnectionClosedError as ex:
-        LOGGER.info(f"ConnectionClosedError from server: {ex}")
+        LOGGER.error(f"ConnectionClosedError from server: {ex}")
     except ConnectionClosed as ex:
-        LOGGER.info(f"ConnectionClosed from server: {ex}")
+        LOGGER.error(f"ConnectionClosed from server: {ex}")
 
-async def input_listener(websocket: ClientConnection):
+async def inputListener(websocket: ClientConnection):
     LOGGER.info("Listening to console for messages...")
     try:
         while True:
@@ -37,11 +38,11 @@ async def input_listener(websocket: ClientConnection):
     except EOFError as ex:
         LOGGER.info("EOFError stopping input listener...")
 
-async def start_client(host: str, port: int):
-    async with connect(f"ws://{host}:{port}") as websocket:
+async def startClient(host: str, port: int, ticket: str):
+    async with connect(f"ws://{host}:{port}", additional_headers={"sec-websocket-protocol": ticket}) as websocket:
         LOGGER.info(f"Connected to {host}:{port} on local port {websocket.local_address[1]}!")
-        in_task = asyncio.create_task(input_listener(websocket))
-        rec_task = asyncio.create_task(ws_listener(websocket))
+        in_task = asyncio.create_task(inputListener(websocket))
+        rec_task = asyncio.create_task(wsListener(websocket))
         _, pending = await asyncio.wait(
             [
                 in_task,
@@ -52,13 +53,24 @@ async def start_client(host: str, port: int):
         for task in pending:
             task.cancel()
 
-def main(host: str, port: int):
+def getTicket(host: str, port: int, band: str) -> str:
+    response = requests.get(f"http://{host}:{port}/gig/ticket?band={band}")
+    if response.status_code != 200:
+        raise Exception(response.json())
+    return response.json()
+
+def main(ws_host: str, ws_port: int, ticket_host: str, ticket_port: int, band: str):
     try:
-        asyncio.run(start_client(host, port))
+        asyncio.run(startClient(ws_host, ws_port, getTicket(ticket_host, ticket_port, band)))
     except KeyboardInterrupt:
         LOGGER.info("ctrl+c stopped client!")
+    except ConnectionRefusedError as ex:
+        LOGGER.error(f"Unable to connect: {ex}")
 
 if __name__ == "__main__":
-    host = "localhost"
-    port = 8080
-    main(host, port)
+    ws_host = "localhost"
+    ws_port = 8080
+    ticket_host = "localhost"
+    ticket_port = 80
+    band = "The Beatles"
+    main(ws_host, ws_port, ticket_host, ticket_port, band)
