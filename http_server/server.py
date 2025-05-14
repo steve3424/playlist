1f6482f1c24dfaf1ee45c4af958146ad84eda5bc
Steve F <stevefrazee123@gmail.com>
2025-05-14 18:00:55 -0400

    add redis startup/shutdown

--- http_server/server.py
+++ http_server/server.py
@@ -6,9 +6,11 @@ import logging.config
 from .logging_conf import LOGGING_CONFIG
 logging.config.dictConfig(LOGGING_CONFIG)
 import argparse
+import asyncio
 import fastapi
 import uvicorn
 from .routers import gig
+from .services import redis_service
 
 app = fastapi.FastAPI()
 app.include_router(gig.router)
@@ -21,6 +23,14 @@ if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--host", type=str, default="0.0.0.0")
     arg_parser.add_argument("--port", type=int, default=80)
+    arg_parser.add_argument("--redis-host", type=str, default="localhost")
+    arg_parser.add_argument("--redis-port", type=int, default=6379)
     args = arg_parser.parse_args()
 
+    # Startup code
+    redis_service.init(args.redis_host, args.redis_port)
+
     uvicorn.run(app, host=args.host, port=args.port)
+
+    # Shutdown code
+    asyncio.run(redis_service.shutdown())
