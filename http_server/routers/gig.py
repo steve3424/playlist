1002f1957aeac38a24a5d193bc1a5ba684475679
Steve F <stevefrazee123@gmail.com>
2025-05-14 18:00:44 -0400

    add ticket to cache w/ ttl

--- http_server/routers/gig.py
+++ http_server/routers/gig.py
@@ -1,12 +1,13 @@
 import logging
 import base64
-import json
 from fastapi import APIRouter, Depends
 from ..middlewares.auth import Authenticate
+from ..services import redis_service
 from common.encryption import CIPHER
 from common.tickets import Ticket
 
 LOGGER = logging.getLogger(f"__main__.{__name__}")
+TICKET_TTL = 5 # seconds
 
 router = APIRouter(prefix="/gig")
 
@@ -15,14 +16,19 @@ async def ticket(
     band: str,
     user_info: dict=Depends(Authenticate(permission_level=1))
 ):
-    # TODO: add to cache
+    global TICKET_TTL
     # TODO: determine if user is allowed to join gig w/ requested
     #       band.
+    ticket = Ticket(
+        user_id=user_info["id"],
+        band=band
+    ).model_dump_json().encode("utf-8")
+
+    await redis_service.client.set(f"{band}:{user_info['id']}", ticket)
+    await redis_service.client.expire(f"{band}:{user_info['id']}", TICKET_TTL)
+
     return base64.b16encode(
         CIPHER.encrypt(
-            Ticket(
-                user_id=user_info["id"],
-                band=band
-            ).model_dump_json().encode("utf-8")
+            ticket
         )
     ).decode(encoding="utf8")
