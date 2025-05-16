030db0eaab8bfc3e45de2060e84e0a3c143ab96a
Steve F <stevefrazee123@gmail.com>
2025-05-16 17:24:14 -0400

    add env file support

--- http_server/server.py
+++ http_server/server.py
@@ -8,6 +8,9 @@ logging.config.dictConfig(LOGGING_CONFIG)
 import argparse
 import fastapi
 import uvicorn
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 from contextlib import asynccontextmanager
 from .routers import band
 from .services import redis_service
@@ -17,6 +20,14 @@ LOGGER = logging.getLogger("playlist")
 @asynccontextmanager
 async def appLife(app: fastapi.FastAPI, *args, **kwargs):
     LOGGER.info("Starting server...")
+
+    env_file_name = f"{kwargs["environment"]}.env"
+    env_file_path = Path(os.path.dirname(__file__), "configs", env_file_name)
+    LOGGER.info(f"Loading configs from '{env_file_name}'...")
+    env_loaded = load_dotenv(dotenv_path=env_file_path)
+    if not env_loaded:
+        LOGGER.error(f"Failed to load environment!")
+
     redis_service.init(kwargs["redis_host"], kwargs["redis_port"])
     yield
     LOGGER.info("Shutting down server...")
@@ -27,10 +38,11 @@ def health() -> dict:
     
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("--host", type=str, default="0.0.0.0")
-    arg_parser.add_argument("--port", type=int, default=80)
-    arg_parser.add_argument("--redis-host", type=str, default="localhost")
-    arg_parser.add_argument("--redis-port", type=int, default=6379)
+    arg_parser.add_argument("--host",        type=str, default="0.0.0.0")
+    arg_parser.add_argument("--port",        type=int, default=80)
+    arg_parser.add_argument("--redis-host",  type=str, default="localhost")
+    arg_parser.add_argument("--redis-port",  type=int, default=6379)
+    arg_parser.add_argument("--environment", type=str, default="dev", choices=["dev", "prod"])
     args = arg_parser.parse_args()
 
     app = fastapi.FastAPI(
@@ -38,6 +50,7 @@ if __name__ == "__main__":
             app,
             redis_host=args.redis_host,
             redis_port=args.redis_port,
+            environment=args.environment
         )
     )
     app.include_router(band.router)
