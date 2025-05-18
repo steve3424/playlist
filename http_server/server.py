419bd4b3902ee4d6d6cebe55fe86c07c25917291
Steve F <stevefrazee123@gmail.com>
2025-05-17 22:36:36 -0400

    remove f string

--- http_server/server.py
+++ http_server/server.py
@@ -27,7 +27,7 @@ async def appLife(app: fastapi.FastAPI, *args, **kwargs):
     LOGGER.info(f"Loading configs from '{env_file_name}'...")
     env_loaded = load_dotenv(dotenv_path=env_file_path)
     if not env_loaded:
-        LOGGER.warning(f"Failed to load environment file!")
+        LOGGER.warning("Failed to load environment file!")
 
     redis_service.init(kwargs["redis_host"], kwargs["redis_port"])
     yield
