e86af795cef92bbb11b8422b60fe2039ee45790b
Steve F <stevefrazee123@gmail.com>
2025-05-19 18:04:10 -0400

    allow http exceptions to raise as is

--- http_server/middlewares/exception.py
+++ http_server/middlewares/exception.py
@@ -1,6 +1,6 @@
 import logging
 from typing import Callable, Awaitable
-from fastapi import Request
+from fastapi import Request, HTTPException
 from fastapi.responses import Response, JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
 
@@ -14,6 +14,11 @@ class ExceptionHandlerGeneral(BaseHTTPMiddleware):
     ) -> Response:
         try:
             return await call_next(request)
+        except HTTPException as ex:
+            # NOTE: we may want to raise this from our application code.
+            #       we don't want to wrap this in 500 error as we do with
+            #       other unhandled exceptions.
+            raise ex
         except Exception as ex:
             LOGGER.exception(ex)
             return JSONResponse({"message": "Something went wrong"}, status_code=500)
