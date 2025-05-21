a5735d7ff0bbeda3fbaf522b1750740f1a42a183
Steve F <stevefrazee123@gmail.com>
2025-05-21 07:37:00 -0400

    update authentication

--- http_server/middlewares/authentication.py
+++ http_server/middlewares/authentication.py
@@ -6,6 +6,7 @@ from typing import Callable, Awaitable
 from fastapi import Request, Response
 from fastapi.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
+from ..services.authorization import User, AppRoles
 
 LOGGER = logging.getLogger(f"playlist.{__name__}")
 
@@ -30,17 +31,18 @@ class Authenticate(BaseHTTPMiddleware):
             global OPEN_ENDPOINTS
             current_env = os.environ["ENV"]
             if current_env in OPEN_ENVS or request.url.path in OPEN_ENDPOINTS:
-                user_info = {}
-                LOGGER.debug(f"Auth skipped for endpoint '{request.url.path}' in env '{current_env}'!")
+                user_info = User(
+                    id=0,
+                    name="unknown",
+                    role=AppRoles.admin
+                )
             else:
-                time_auth_start = time.perf_counter()
                 if request.headers.get("Authorization", None):
                     user_info = self.validateToken(request.headers["Authorization"])
                 elif request.cookies.get("Authorization", None):
                     user_info = self.validateToken(request.cookies["Authorization"])
                 else:
                     return JSONResponse({"message": "No auth token found!"}, status_code=401)
-                LOGGER.info(f"Auth took {time.perf_counter() - time_auth_start}s!")
             request.state.user_info = user_info
             return await call_next(request)
         except AuthenticationError as ex:
