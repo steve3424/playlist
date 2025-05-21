019897db5b0f5afaefb8fc61c01c1fc4c007c532
Steve F <stevefrazee123@gmail.com>
2025-05-21 12:37:36 -0400

    add roles enum table

--- http_server/data/playlist_schema.sql
+++ http_server/data/playlist_schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS users (
     updated_ts INTEGER NOT NULL DEFAULT (strftime('%s', 'now'))
 );
 
-CREATE TRIGGER updated_ts
+CREATE TRIGGER IF NOT EXISTS updated_ts_users
 AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
@@ -17,3 +17,29 @@ BEGIN
     SET updated_ts = (strftime('%s', 'now'))
     WHERE id = OLD.id;
 END;
+
+----------------------------------------------------------------
+---------------------------- ROLES -----------------------------
+----------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS roles (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(16) UNIQUE NOT NULL,
+    created_ts INTEGER NOT NULL DEFAULT (strftime('%s', 'now')),
+    updated_ts INTEGER NOT NULL DEFAULT (strftime('%s', 'now'))
+);
+
+CREATE TRIGGER IF NOT EXISTS updated_ts_roles
+AFTER UPDATE ON roles
+FOR EACH ROW
+BEGIN
+    UPDATE roles
+    SET updated_ts = (strftime('%s', 'now'))
+    WHERE id = OLD.id;
+END;
+
+-- NOTE: This much match http_server enum values!!
+INSERT OR IGNORE INTO roles
+    (id, name)
+VALUES
+    (0, 'user'),
+    (1, 'admin');
