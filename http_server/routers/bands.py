33832d7d523adb19dd91ab3f3c0b5336cb6912b3
Steve F <stevefrazee123@gmail.com>
2025-05-22 14:31:03 -0400

    update band app auth

--- http_server/routers/bands.py
+++ http_server/routers/bands.py
@@ -1,9 +1,8 @@
 import logging
-import time
 from fastapi import APIRouter, Depends, Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import PlainTextResponse
 from common import tickets
-from common.tickets import TicketError
+from ..services.authorization import AuthorizeAppRole, AppRoles, User
 
 LOGGER = logging.getLogger(f"playlist.{__name__}")
 
@@ -13,14 +12,9 @@ router = APIRouter(prefix="/band")
 async def ticket(
     request: Request,
     band: str,
-    user_info: dict={"name": "steve"}
-):
-    try:
-        time_ticket_start = time.perf_counter()
-        # TODO: determine if user is allowed to have ticket for requested band.
-        ticket = await tickets.create(user_info["name"], band)
-        LOGGER.info(f"{request.url.path} took {time.perf_counter() - time_ticket_start}s!")
-        return ticket
-    except TicketError as ex:
-        LOGGER.exception(ex)
-        return JSONResponse({"message": "Error getting ticket!"}, status_code=500)
+    user_info: User=Depends(AuthorizeAppRole(AppRoles.user))
+) -> PlainTextResponse:
+    # TODO: determine if user is allowed to have ticket for requested band.
+    ticket = await tickets.create(user_info.name, band)
+    LOGGER.debug(ticket)
+    return PlainTextResponse(ticket)
