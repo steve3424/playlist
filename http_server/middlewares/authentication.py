2091f88c04af5b9e05e26bc55b77d18c3e59b920
Steve F <stevefrazee123@gmail.com>
2025-06-19 11:19:46 -0400

    open access to openapi.json

--- http_server/middlewares/authentication.py
+++ http_server/middlewares/authentication.py
@@ -49,7 +49,10 @@ class Authenticate(BaseHTTPMiddleware):
         call_next: Callable[[Request], Awaitable[Response]]
     ) -> Response:
         try:
-            if request.url.path == "/docs" and request.method.upper() == "GET":
+            if (
+                (request.url.path == "/docs" and request.method.upper() == "GET") or
+                (request.url.path == "/openapi.json" and request.method.upper() == "GET")
+            ):
                 return await call_next(request)
             elif request.url.path == "/users/sessions" and request.method.upper() == "POST":
                 response = await call_next(request)
