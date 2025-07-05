a5b09da5810a01f7ca86c3a79a70412662628846
Steve F <stevefrazee123@gmail.com>
2025-07-05 18:43:47 -0400

    fix create_db.bat

--- create_db.bat
+++ create_db.bat
@@ -3,7 +3,8 @@
 IF "%~1" == "" (
     echo Please specify db name!
 ) ELSE (
+    SETLOCAL EnableDelayedExpansion
     SET DB_NAME=http_server\data\playlist_%1.db
-    sqlite3 %DB_NAME% < http_server\data\playlist_schema.sql
-    echo Created db at '%DB_NAME%'!
+    sqlite3 !DB_NAME! < http_server\data\playlist_schema.sql
+    echo Created db at '!DB_NAME!'!
 )
