f2af23e5a84b3df5d3d8bfe1d65b2b67d5ce3c89
Steve F <stevefrazee123@gmail.com>
2025-11-01 13:58:45 -0400

    throw startup error if user auth function is omitted, remove bands endpoints

--- http_server/authorization/endpoint.py
+++ http_server/authorization/endpoint.py
@@ -12,9 +12,11 @@ class AuthorizationError(Exception):
         super().__init__(message)
 
 class AuthorizeEndpoint:
-    def __init__(self, role: AppRoles, endpoint_authorization: Callable | None=None):
-        self.role = role
+    def __init__(self, role: AppRoles=None, endpoint_authorization: Callable | None=None):
+        self.role = role if role is not None else AppRoles.admin
         self.endpoint_authorization = endpoint_authorization
+        if self.role.value < AppRoles.admin.value and self.endpoint_authorization == None:
+            raise Exception(f"Endpoint allows role '{self.role.name}', but has no further authorizations!")
 
     def __call__(
         self,
@@ -24,7 +26,5 @@ class AuthorizeEndpoint:
         if user_info.role < self.role:
             raise AuthorizationError()
         elif user_info.role < max(e.value for e in AppRoles):
-            if not self.endpoint_authorization:
-                raise Exception("User level permissions must have further authorizations!")
             self.endpoint_authorization(request, user_info)
         return user_info
