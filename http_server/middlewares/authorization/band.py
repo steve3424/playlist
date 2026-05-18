052e1f97d873b18b0a3e6db3013c51b39f393fff
stevef <stevefrazee123@gmail.com>
2026-05-18 12:38:32 -0400

    admin does not call auth functions

--- http_server/middlewares/authorization/band.py
+++ http_server/middlewares/authorization/band.py
@@ -1,12 +1,11 @@
 from fastapi import Request
-from .models import User, AppRoles
+from .models import User
 from .main import AuthorizationError
 from ...data import db
 
 BAND_CREATE_LIMIT = 10
 
 async def createLimitReached(request: Request, user_info: User):
-    if user_info.role < AppRoles.admin:
-        created_count = await db.bandCountCreated(user_info.id)
-        if BAND_CREATE_LIMIT <= created_count[0]["count"]:
-            raise AuthorizationError(f"Cannot create more than {BAND_CREATE_LIMIT} bands!")
+    created_count = await db.bandCountCreated(user_info.id)
+    if BAND_CREATE_LIMIT <= created_count[0]["count"]:
+        raise AuthorizationError(f"Cannot create more than {BAND_CREATE_LIMIT} bands!")
