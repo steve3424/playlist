c2a76644a83eb2fc586325b6c89eaa7d2e7b21e3
stevef <stevefrazee123@gmail.com>
2026-05-18 21:02:25 -0400

    update db create script

--- create_db.bat
+++ create_db.bat
@@ -4,7 +4,7 @@ IF "%~1" == "" (
     echo Please specify db name!
 ) ELSE (
     SETLOCAL EnableDelayedExpansion
-    SET DB_NAME=http_server\data\playlist_%1.db
+    SET DB_NAME=http_server\data\%1
     sqlite3 !DB_NAME! < http_server\data\playlist_schema.sql
     echo Created db at '!DB_NAME!'!
 )
